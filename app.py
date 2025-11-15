1ea8271f407958b4958e14f775d1db94d0255f93
jwiniarz03 <winiarz.julia@icloud.com>
2025-11-15 19:37:58 +0100

    feat(ui): add initial gradio app structure

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-import gradio as gd
+import gradio as gr
+import os
+from src.pipeline import analyze_report_file
+
+example_file_path = os.path.join(os.getcwd(), "data")
+example_1 = os.path.join(example_file_path, "Wydatki.pdf")
+example_2 = os.path.join(example_file_path, "Wydatki.png")
+
+demo = gr.Interface(
+    fn=analyze_report_file,
+    inputs=gr.File(label="File"),
+    outputs=[
+        gr.Textbox(label="Extracted Data"),
+        gr.Textbox(label="Key Insights"),
+        gr.Textbox(label="Conclusion"),
+    ],
+    examples=[example_1, example_2],
+    title="Chart Analysis Assistant",
+    description="Upload a PNG or PDF file with a chart and get detailed analysis and insights.",
+    live=True,
+)
+
+
+if __name__ == "__main__":
+    demo.launch()
